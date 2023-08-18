e18955f1c0a284e9c74c40b683a30905d8297aa2
ranjanmehto6 <ranjanmehto6@gmail.com>
2023-08-18 15:09:00 +0530

    Fourth

--- src/main/java/com/example/myblog7/Myblog7Application.java
+++ src/main/java/com/example/myblog7/Myblog7Application.java
@@ -1,13 +1,24 @@
 package com.example.myblog7;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Myblog7Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Myblog7Application.class, args);
+
+
+    }
+
+    //We create bean when we want use external library is added for which IOC unable to create object we use
+    // @Bean annotation
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
     }
 
 }
